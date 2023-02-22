5b1e731a26ed291836cbb548373e385489e8fd8d
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2023-02-22 13:42:19 +0200

    initial commit template

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+import icons from "./icons"; //get access to all icons ( in object)
+
+import theme, { COLORS, SIZES, FONTS } from "./theme"; //access to appTheme object and access the named exports which are fonts, sizes and color) 
+
+export { icons,  theme, COLORS, SIZES, FONTS }; //add all icons object in one object. add the
---
+++ navigation/Topnavigator.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Topnavigator = () => {
+  return (
+    <View>
+      <Text>Topnavigator</Text>
+    </View>
+  )
+}
+
+export default Topnavigator
+
+const styles = StyleSheet.create({})
