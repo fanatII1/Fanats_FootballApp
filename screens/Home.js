d4d848a620e0faf9d5cbc198a4c8847746c02702
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2023-02-27 14:32:45 +0200

    added home header

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react';
+import { StyleSheet, TouchableOpacity, Text, View, StatusBar, SafeAreaView, ImageBackground, Image, TextInput } from 'react-native';
 import { teams, fixtures, matchOfTheDay } from '../data/footballData'
+import { AntDesign, Entypo, EvilIcons } from '@expo/vector-icons';
+import { icons,  theme, COLORS, SIZES, FONTS} from '../constants/index';
+import { Dimensions } from 'react-native';
+import { Shadow } from 'react-native-shadow-2';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.HomeContainer}>
+      {/*HEADER*/}
+      <View style={styles.headerWrapper}>
+        <View>
+          <EvilIcons name="navicon" size={26} color="black" />
+        </View>
+        <View style={styles.logoWrapper}>
+          <Image source={require('../assets/images/Logo.png')} resizeMode='contain' style={styles.logo}/>
+          <Text style={styles.appHeaderText}>Fanat Scouts</Text>
+        </View>
+        <View>
+          <AntDesign name="search1" size={22} color={'black'} />
+        </View>
+      </View>
+
+      
+    </SafeAreaView>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  HomeContainer:{
+    flex: 1,
+    backgroundColor: COLORS.text
+  },
+  headerWrapper:{
+    height: "10%",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginHorizontal: 15,
+  },
+  logoWrapper:{
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+  logo:{
+    width: 40,
+    height: 40,
+    borderRadius: 40/2,
+    marginRight: 10
+  },
+  appHeaderText:{
+    color: "#000",
+    fontSize: 16
+  }
+})
