b93fad12b5cceed87625729fc4ed9a742d016ce0
georgemarwanqana@gmail.com <georgemarwanqana@gmail.com>
2023-03-02 15:18:54 +0200

    added bg color change when click flatlist

--- screens/Home.js
+++ screens/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import { StyleSheet, TouchableOpacity, Text, View, StatusBar, SafeAreaView, ImageBackground, Image, TextInput, ScrollView } from 'react-native';
 import { teams, fixtures, results, sortedStandings, matchOfTheDay } from '../data/footballData'
 import { AntDesign, Entypo, EvilIcons } from '@expo/vector-icons';
@@ -10,20 +10,20 @@ import { useNavigation } from '@react-navigation/native';
 import { FlatList } from 'react-native-gesture-handler';
 
 const Home = () => {
-  // console.log(matchOfTheDay)
+  const [matchIndex, setMatchIndex] = useState(0)
 
   function renderHeader(){
     return(
       <>
       <View style={styles.header_wrapper}>
         <View style={{flex: 1}}>
-          <EvilIcons name="navicon" size={26} color="#fff" />
+          <EvilIcons name='navicon' size={26} color='#fff' />
         </View>
         
         <Image source={require('../assets/images/Logo_2.png')} resizeMode='contain' style={styles.logo}/>
         
         <View style={{flex: 1, alignItems: 'flex-end',}}>
-          <AntDesign name="search1" size={22} color="#fff" style={{backgroundColor: COLORS.support_primary, padding: 7, borderTopRightRadius: 12}} />
+          <AntDesign name='search1' size={22} color='#fff' style={{backgroundColor: COLORS.support_primary, padding: 7, borderTopRightRadius: 12}} />
         </View>
       </View>
 
@@ -35,7 +35,7 @@ const Home = () => {
           renderItem={({item, index})=>{
             
             return (
-                <TouchableOpacity style={styles.teamWrapper}>
+                <TouchableOpacity activeOpacity={0.8} style={styles.teamWrapper}>
                     <Image source={item.team.logo} resizeMode='contain' style={styles.badge}/>
                 </TouchableOpacity>
             )
@@ -45,7 +45,7 @@ const Home = () => {
       </View>
 
       <Text style={styles.sectionHeading}>
-        <Entypo name="dot-single" size={24} color="#925BFF" />
+        <Entypo name='dot-single' size={24} color='#925BFF' />
         Live Matches
       </Text>
 
@@ -56,14 +56,17 @@ const Home = () => {
         horizontal
         showsHorizontalScrollIndicator={false}
         renderItem={({item, index})=>{
+          //we set the gradient colot of the component based on the component(index) clicked
+          let firstColor = index === matchIndex ? COLORS.secondary : '#232232';
+          let secondColor = index === matchIndex ? COLORS.secondary : '#232232';
             
           return (
-              <TouchableOpacity style={styles.liveGame}>
+              <TouchableOpacity activeOpacity={0.6} style={styles.liveGame} onPress={()=> setMatchIndex(index)}>
                 <LinearGradient
-                  colors={['#2a2e3a', '#2a2e3a']}
-                  start={{x: 0, y: 0}}
+                  colors={[firstColor, secondColor]}
+                  start={{x: 0, y: 1}}
                   end={{x: 1, y: 1}}
-                  style={{height: '100%', width: "100%", borderRadius: 20, borderColor: COLORS.secondary, borderWidth: 4}}
+                  style={{height: '100%', width: '100%', borderRadius: 20,}}
                 >
                   <View style={styles.tournamentContainer}>
                     <View style={styles.tournamentIconWrapper}>
@@ -85,7 +88,7 @@ const Home = () => {
       />
 
       <Text style={styles.sectionHeading}>
-        <Entypo name="dot-single" size={24} color="#925BFF" />
+        <Entypo name='dot-single' size={24} color='#925BFF' />
         Match Schedule
       </Text>
 
@@ -99,11 +102,11 @@ const Home = () => {
     return(
       <>
       <Text style={styles.sectionHeading}>
-        <Entypo name="dot-single" size={24} color="#925BFF" />
+        <Entypo name='dot-single' size={24} color='#925BFF' />
         Standings
       </Text>
-      <View style={{backgroundColor: '#232232', marginHorizontal: 5, paddingHorizontal: 10, borderRadius: 20}}>
-        <Text style={{color: "#fff", fontSize: 20, marginVertical: 17}}> Table standings </Text>
+      <View style={{backgroundColor: '#fff', marginHorizontal: 5, marginBottom: 10, paddingHorizontal: 10, borderRadius: 7}}>
+        <Text style={{color: '#000', fontSize: 20, marginVertical: 17}}> Table standings </Text>
         <FlatList
           data={sortedStandings}
           showsVerticalScrollIndicator={false}
@@ -111,31 +114,31 @@ const Home = () => {
             return (
               <View style={{flexDirection: 'row', marginHorizontal: 5, borderBottomColor: '#fff', borderBottomWidth: 1}}>
                 <View style={{flex: 1.2}}>
-                  <Text style={{fontSize: 18, fontWeight: 'bold', color: COLORS.text}}>Club</Text>
+                  <Text style={{fontSize: 18, fontWeight: 'bold', color: '#000'}}>Club</Text>
                 </View>
                 <View style={{flex: 0.8, flexDirection: 'row', justifyContent: 'space-between'}}>
-                  <Text style={{fontSize: 18, fontWeight: 'bold', color: COLORS.text}}>P</Text>
-                  <Text style={{fontSize: 18, fontWeight: 'bold', color: COLORS.text}}>W</Text>
-                  <Text style={{fontSize: 18, fontWeight: 'bold', color: COLORS.text}}>L</Text>
-                  <Text style={{fontSize: 18, fontWeight: 'bold', color: COLORS.text}}>Pts</Text>
+                  <Text style={{fontSize: 18, fontWeight: 'bold', color: '#000'}}>P</Text>
+                  <Text style={{fontSize: 18, fontWeight: 'bold', color: '#000'}}>W</Text>
+                  <Text style={{fontSize: 18, fontWeight: 'bold', color: '#000'}}>L</Text>
+                  <Text style={{fontSize: 18, fontWeight: 'bold', color: '#000'}}>Pts</Text>
                 </View>
               </View>
             )
           }}
           renderItem={({item, index}) => {
-            console.log(index)
+            // console.log(index)
             if(index <= 5){
               return (
                 <View style={{flexDirection: 'row', paddingVertical: 10, borderBottomColor: '#fff', borderBottomWidth: 1 }}>
                   <View style={{flex: 1.2, flexDirection: 'row', alignItems: 'center'}}>
                     <Image style={{height: 30, width: 40}} source={item.logo} resizeMode='contain'/>
-                    <Text style={{color: COLORS.text, marginLeft: 10}}>{item.name}</Text>
+                    <Text style={{color: '#000', marginLeft: 10}}>{item.name}</Text>
                   </View>
                   <View style={{flex: 0.8, flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <Text style={{fontSize: 18, color: COLORS.text}}>{item.standings.plays}</Text>
-                    <Text style={{fontSize: 18, color: COLORS.text}}>{item.standings.wins}</Text>
-                    <Text style={{fontSize: 18, color: COLORS.text}}>{item.standings.loses}</Text>
-                    <Text style={{fontSize: 18, color: COLORS.text}}>{item.standings.pts()}</Text>
+                    <Text style={{fontSize: 18, color: '#000'}}>{item.standings.plays}</Text>
+                    <Text style={{fontSize: 18, color: '#000'}}>{item.standings.wins}</Text>
+                    <Text style={{fontSize: 18, color: '#000'}}>{item.standings.loses}</Text>
+                    <Text style={{fontSize: 18, color: '#000'}}>{item.standings.pts()}</Text>
                   </View>
                 </View>
               )
@@ -144,6 +147,11 @@ const Home = () => {
           keyExtractor={(team, index) => index}
         />
       </View>
+
+      <Text style={styles.sectionHeading}>
+        <Entypo name='dot-single' size={24} color='#925BFF' />
+        Latest News
+      </Text>
       </>
     )
   }
@@ -156,20 +164,20 @@ const Home = () => {
         ListHeaderComponent={renderHeader}
         renderItem={({item, index}) =>{
           return(
-            <TouchableOpacity style={{ flexDirection: 'row', margin: 5, height: 85, backgroundColor: '#232232', borderRadius: 10, overflow: 'hidden'}}>
+            <TouchableOpacity activeOpacity={0.8} style={{ flexDirection: 'row', margin: 5, height: 85, backgroundColor: '#fff', borderRadius: 10, overflow: 'hidden'}}>
               <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-start', flex: 1,}}>
-                <Text style={{marginLeft: 10, color: '#fff'}}>{item.teams.home.name}</Text>
+                <Text style={{marginLeft: 10, color: '#000'}}>{item.teams.home.name}</Text>
                 <Image style={{marginLeft: 10, height: 28, width: 30}} source={item.teams.home.logo} resizeMode='contain'/>
               </View>  
 
               <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', paddingLeft: 5}}>
-                <Text style={{color:'#fff'}}>27 Aug</Text>
-                <Text style={{color:'#fff'}}>{item.fixture.time}</Text>
+                <Text style={{color:'#000'}}>27 Aug</Text>
+                <Text style={{color:'#000'}}>{item.fixture.time}</Text>
               </View>
 
               <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', flex: 1}}>
                 <Image style={{height: 18, width: 30}} source={item.teams.away.logo} resizeMode='contain'/>
-                <Text style={{color: '#fff', marginRight: 10}}>{item.teams.away.name}</Text>
+                <Text style={{color: '#000', marginRight: 10}}>{item.teams.away.name}</Text>
               </View>
             </TouchableOpacity>
           )
@@ -207,12 +215,12 @@ const styles = StyleSheet.create({
     color: COLORS.text
   },
   app_headerText:{
-    color: "#000",
+    color: '#000',
     fontSize: 20,
     fontWeight: '400'
   }, 
   teamWrapper: {
-    backgroundColor: "#fff", 
+    backgroundColor: '#fff', 
     marginRight: 15,
     height: 45, 
     width: 45, 
@@ -225,8 +233,8 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
   },
   badge:{
-    height: "65%",
-    width: "90%",
+    height: '65%',
+    width: '90%',
   },
   top_match_wrapper: { 
     alignItems: 'center',
@@ -238,7 +246,7 @@ const styles = StyleSheet.create({
   },
   tournamentContainer: {
     position: 'absolute', 
-    width: "100%", 
+    width: '100%', 
     top: -10, 
     justifyContent: 'center', 
     alignItems: 'center'
@@ -247,23 +255,23 @@ const styles = StyleSheet.create({
     height: 35, 
     width: 35, 
     borderRadius: 35/2, 
-    backgroundColor: "#fff", 
+    backgroundColor: '#fff', 
     justifyContent: 'center', 
     alignItems: 'center'
   },
   tournamentIcon: {
-    height: "95%", 
-    width: "95%"
+    height: '95%', 
+    width: '95%'
   },
   liveTeamsContainer: {
     flexDirection: 'row', 
     justifyContent: 'center',
     alignItems: 'center', 
-    height: "50%",
+    height: '50%',
   },
   liveTeam: {
-    height: "50%", 
-    width: "50%",
+    height: '50%', 
+    width: '50%',
     marginTop: 25
   },
   liveTeamName: {
